e4c95669c6ecfe2b2945fb0c9286547c2776d932
Andree Jimenez <andree_jimenez@outlook.com>
2020-04-05 16:37:05 -0500

    Update dependencies.

--- AppTrips.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ AppTrips.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -70,7 +70,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"acdb03da-d1a8-4fb7-976a-11a98f75396f"
+	.asciz	"7f594bfc-f84e-48c0-b123-b40d14c66082"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
