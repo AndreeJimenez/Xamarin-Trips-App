773cd104572c560601852b87556fc4a2430326d9
Andree Jimenez <andree_jimenez@outlook.com>
2020-04-06 00:27:37 -0500

    Design UI

--- AppTrips.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ AppTrips.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -89,7 +89,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"b1266cb4-8070-41a9-b5ad-820a9d749831"
+	.asciz	"53235fa5-43c2-460e-b71e-6e9743a8922c"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
